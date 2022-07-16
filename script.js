7eed4778158a65440af7a531abb38e33e7347f21
Andres David Blandon <dandresavid@hotmail.com>
2022-07-16 11:31:48 -0500

    Changed slider buttons effect

--- script.js
+++ script.js
@@ -439,6 +439,20 @@ setTimeout(() => h1.removeEventListener(`mouseenter`,alerrtH1)
 //   if(el !== h1) el.style.transform = `scale(0.5)`;
 // })
 
+document.addEventListener('DOMContentLoaded', function(e){
+  console.log('HTML parsed and DOM tree built!', e);
+})
+
+window.addEventListener('load', function(e){
+  console.log('Page fully loaded', e);
+})
+// window.addEventListener('beforeunload', function(e){
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// })
+
+
 
 
 
